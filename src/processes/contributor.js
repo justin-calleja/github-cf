b6b4d559e4086c0867588652bfc91de6f3ce9f15
Justin Calleja <calleja.justin@gmail.com>
2017-05-26 22:36:58 +0200

    Add concurrency

--- src/processes/contributor.js
+++ src/processes/contributor.js
@@ -9,12 +9,18 @@ function * contributorProcess () {
   const msg = yield csp.take(contributorCh)
   if (ResponseMsg.is(msg)) {
     const { json } = msg
+    let doneChs = []
     for (let i = 0; i < json.length; i++) {
       const contributor = json[i]
       yield csp.put(outputCh, AddContributor(contributor.login, contributor.html_url))
-      const doneCh = csp.go(followerProcess, [contributor.followers_url, contributor.login, csp.chan()])
-      yield csp.take(doneCh)
+      doneChs.push(
+        csp.go(followerProcess, [contributor.followers_url, contributor.login, csp.chan()])
+      )
     }
+
+    for (let doneCh of doneChs) { yield doneCh }
+
+    // all followerProcesses have ended
     return yield csp.put(outputCh, FlushMsg())
   } else {
     console.error('WARNING: received unexpected message in contributorCh. Message:', msg)
